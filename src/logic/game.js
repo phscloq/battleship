0874608e258ba503daad9f7f142baf2b577a439e
Baran Dogan <phscloq@gmail.com>
2023-04-10 17:35:39 +0300

    turn based attack updated and announcement added

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,33 +1,5 @@
-/* const ship = new Ship('carrier');
-const ship1 = new Ship('battleship');
-const ship2 = new Ship('cruiser');
-const ship3 = new Ship('submarine');
-const ship4 = new Ship('destroyer');
-const ships=[ship, ship1, ship2, ship3, ship4];
-const ships2= [ship, ship1, ship2, ship3, ship4];
-const userBoard = new Board(10, 10, ships, ships.length);
-const compBoard = new Board(10, 10, ships2, ships2.length);
-userBoard.placeShip(ship, 2, 1, 'right');
-userBoard.placeShip(ship1, 0,5, 'right');
-userBoard.placeShip(ship2, 7, 1, 'right');
-userBoard.placeShip(ship3, 5,7, 'bottom'); 
-userBoard.placeShip(ship4, 9, 5, 'right');
+import { hidden, show, endScreen} from "./dom.js";
 
-compBoard.placeShip(ship, 3,0, 'right');
-compBoard.placeShip(ship1, 5, 7, 'bottom');
-compBoard.placeShip(ship2, 7,2, 'bottom');
-compBoard.placeShip(ship3, 1, 7, 'right');
-compBoard.placeShip(ship4, 0,2, 'right');
-
-const user1= new Player('Ben', userBoard);
-const user2= new Player('comp', compBoard);
-const game=new Game(user1,user2,userBoard, compBoard); */
-console.log("Working"); 
-import { Ship } from "./ship.js";
-import { Board } from "./gameboard.js";
-import { Player } from "./player.js";
-import { hidden, show } from "./dom.js";
-let turn=0;
 let hitRow; let r; let c;
 let hitColmn;
 class Game{
@@ -39,57 +11,79 @@ class Game{
         this.playType=playType; 
     }
     async play(){
+        
         let currentPlayer=this.player1;
         let currentBoard = this.board2;
         hidden('cBoard');
+        let clicked=true;
+      
         while(!this.gameEnd(currentBoard, currentPlayer)){
-        
-            if(currentPlayer==this.player1){
-           
-                const {row, colmn}= await this.getUserClick(currentBoard);
-                if(this.player1.attack(currentBoard, row, colmn)){
-                    this.gameEnd(currentBoard, currentPlayer);
-                    
-                    break;
-                }
-                currentPlayer=this.player2;
-                currentBoard=this.board1;
 
-            }
-            else{
-                if(this.playType=='vsAI'){
-                this.aiMove(currentBoard, currentPlayer);
+            const {player, targetBoard} = this.turn(currentPlayer, currentBoard);
+            
+
+            const {row, colmn}= await this.getUserClick(currentBoard);
+            console.log("currentboard and user is:");
+            console.log(currentBoard);
+            console.log(currentPlayer);
+           
+            console.log("*****************");
+
+                document.querySelector('#finishedMove').addEventListener('click', ()=>{
+                    clicked=true;
+                    if(currentPlayer==this.player1){
+                     
+                        hidden('cBoard');
+                        document.querySelector('#finishedMove').textContent= "Curtain!";
+                      
+                        setTimeout(()=>{show(this.board1)}, 1000);
+                        setTimeout(()=>{document.querySelector('#finishedMove').textContent='Finished Move'}, 1000);
+                        setTimeout(()=>{document.getElementById('moveText').textContent=`${currentPlayer.playerName} is going to make a move`;
+                    }, 1000)
+
+                    } else if(currentPlayer==this.player2){
+                      
+                        hidden('pBoard');
+                        
+                        
+                        document.querySelector('#finishedMove').textContent= "Curtain!";
+                        setTimeout(()=>{show(this.board2)}, 1000);
+                        setTimeout(()=>{document.querySelector('#finishedMove').textContent='Finished Move'}, 1000);
+                        setTimeout(()=>{document.getElementById('moveText').textContent=`${currentPlayer.playerName} is going to make a move`;
+                    }, 1000)
+                    }
+        
+                });
+                if(clicked==true){
+                   
+                    document.getElementById('moveText').textContent=`${currentPlayer.playerName} is attacking...`;
+                if(currentPlayer.attack(currentBoard, row, colmn)){
+                    this.gameEnd(currentBoard, currentPlayer);           
+                    currentPlayer=player;              
+                    currentBoard=targetBoard;
+                    clicked=false;
                 }
-                else if(this.playType=='vsP2'){
-                    console.log("Player 2's turn")
-                    console.log(currentBoard);
-                    const {row, colmn}= await this.getUserClick(currentBoard);
-                    if(this.player2.attack(currentBoard, row, colmn)){
-                        this.gameEnd(currentBoard, currentPlayer);
-                        break;
-                    }  
-                    currentPlayer=this.player1;
-                    currentBoard=this.board2; 
-                }
-               
-     
-            }
-            document.querySelector('#finishedMove').addEventListener('click', ()=>{
-                if(currentPlayer==this.player1){
-                    hidden('pBoard');
-                    show(this.board1);
-                 
-                }
-                else if(currentPlayer==this.player2){
-                    hidden('cBoard');
-                    show(this.board2);
+                    /* notClickable(currentBoard, row, colmn); */
                     
+
                 }
-            });
         }
+    }
+
+    getUserClick(currentBoard){
+        return new Promise(resolve=>{
+            document.getElementById(currentBoard.id).addEventListener('click', (e)=>{
+            
+                const row =  e.target.getAttribute('datax');
+                const colmn =  e.target.getAttribute('datay');
+                resolve({row, colmn});
+        })
+        })
+       
+}
        
       
-    }
+    
     aiMove(currentBoard, currentPlayer){
         
         if(hitRow==undefined){
@@ -110,23 +104,30 @@ class Game{
         }
         console.log(currentBoard.missAttacks);
     }
-    getUserClick(currentBoard){
-        return new Promise(resolve=>{
-            document.getElementById(currentBoard.id).addEventListener('click', (e)=>{
+ 
+        turn(player, targetBoard){
+            console.log("turn called")
+            if(player==this.player1){
+               
+                player=this.player2;
+                targetBoard=this.board1;
+                return {player, targetBoard};
+                
+            }
+            else if(player==this.player2){
+                player=this.player1;
+                targetBoard=this.board2;
+                
+                return {player, targetBoard};
+                
+            }
+           
             
-                const row =  e.target.getAttribute('datax');
-                const colmn =  e.target.getAttribute('datay');
-                resolve({row, colmn});
-        })
-        })
-       
-}
+        }
     gameEnd(board, winner){
         
-       if(board.isAllShipsSunked()){
-        console.log("Game Over");
-        console.log(winner);
-        console.log(board);
+       if(board.isAllShipsSunked(board)){
+        endScreen();
         return true;
        }
     }
