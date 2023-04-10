5a1a6827b3ee9a46c981f060987b5b28f41cc40f
Baran Dogan <phscloq@gmail.com>
2023-04-10 17:22:45 +0300

    now user can't attack the cell which already been attacked in previous rounds

--- src/logic/player.js
+++ src/logic/player.js
@@ -2,10 +2,22 @@ class Player{
     constructor(playerName, board){
         this.playerName = playerName;
         this.board=board;
+        this.attackedCells=[];
     }
      attack(enemyBoard, row, colmn){
-      
-          return enemyBoard.receiveAttack(enemyBoard, row, colmn);
+        
+        if(this.attackedCells.some((e)=>e[0]==row && e[1]==colmn)){
+            console.log("This cell has already been attacked!");
+            document.getElementById('moveText').textContent=`This cell has already been attacked`;
+            setTimeout(()=>{document.getElementById('moveText').textContent=`Please make a new move!`},1000);
+
+            return false;
+        }
+        else{
+            this.attackedCells.push([row, colmn]);
+            return setTimeout(()=>{enemyBoard.receiveAttack(enemyBoard, row, colmn)},1000);
+
+        }
          
         
     }
