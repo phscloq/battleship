b15b3209d574ed47d6f4123ee9658abbe8a7ea74
Baran Dogan <phscloq@gmail.com>
2023-04-10 17:31:31 +0300

    end screen updated: now could restart a new game and when a ship is sunked we see its type

--- src/logic/dom.js
+++ src/logic/dom.js
@@ -1,6 +1,7 @@
 import { Game } from "./game";
 import { Board } from "./gameboard";
-
+import { createBoard } from "./default";
+import { newGame } from "..";
 function hitDom(board, row, colmn){
     const cell=  document.querySelector(`[datax="${row}"][datay="${colmn}"][id="${board.id}"]`);
     cell.classList.add('hit');
@@ -9,15 +10,51 @@ function missDom(board, row, colmn){
     const cell=  document.querySelector(`[datax="${row}"][datay="${colmn}"][id="${board.id}"]`);
     cell.classList.add('missed');
 }
-function endScreen(winner){
+function endScreen(player, computer, userBoard, compBoard){
+    console.log('endScreen called');
+    document.getElementById('finishedMove').style.display='none';
+    document.getElementById('rematch').style.display='block';
+/*     const main = document.getElementById('main'); */
+    if(document.getElementById('rematch')){
+        const rematch= document.querySelector('#rematch');
+    rematch.addEventListener('click', ()=>{ 
+        console.log('clicked');
+       
+        
+       
+     clearBoard(userBoard);
+     clearBoard(compBoard);
+        newGame();
+    });
+    
+    }
 
 }
+function clearBoard(board){
+  
+    for(let i=0; i<board.userMap.length; i++){
+        for(let j=0; j<board.userMap[i].length; j++){
+            if(board.userMap[i][j].occupied==true){
+                const cell= document.querySelector(`[datax="${i}"][datay="${j}"][id="${board.id}"]`);
+            if(cell.classList.contains('hit')){
+                cell.classList.remove('hit');
+            }
+            else if(cell.classList.contains('missed')){
+                cell.classList.remove('missed');
+            }
+            }}}
+
+        
+}
+
 function hidden(board){
     console.log(board);
 const boardDiv = document.getElementById(board);
 for(let i=0; i<boardDiv.childNodes.length; i++){
-
+    if(!boardDiv.childNodes[i].classList.contains('sunked')){
+        
     boardDiv.childNodes[i].innerHTML='';
+    }
     boardDiv.childNodes[i].classList.remove('occupied');
 }
 }
@@ -38,4 +75,16 @@ function show(board){
         }
 }
     }
-export {hitDom, missDom, hidden, show};
+    function sunkedShip(board, hitsArr, ship){
+        console.log(board);
+        console.log(hitsArr);
+        for(let i=0; i<hitsArr.length; i++){
+           let cell= document.querySelector(`[datax="${hitsArr[i][0]}"][datay="${hitsArr[i][1]}"][id="${board.id}"]`);
+            cell.innerText = ship.type;
+            cell.classList.add('sunked');
+        }
+    }
+
+
+
+export {hitDom, missDom, hidden, endScreen, show, sunkedShip};
